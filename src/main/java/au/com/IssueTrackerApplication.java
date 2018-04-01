1fa9d0ab734f78f949ab902ad6c1c5276bcd0892
Amit S. Joshi <amitsjoshi@gmail.com>
2018-04-01 16:11:40 +1000

    Third check-in

--- src/main/java/au/com/IssueTrackerApplication.java
+++ src/main/java/au/com/IssueTrackerApplication.java
@@ -1,15 +1,9 @@
 package au.com;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@EnableJpaRepositories("au.com.repository")
-@EntityScan("au.com.domain")
 public class IssueTrackerApplication {
 
     public static void main(final String[] args) {
--- src/main/java/au/com/controller/IssueTrackerController.java
+++ src/main/java/au/com/controller/IssueTrackerController.java
@@ -1,9 +1,9 @@
 package au.com.controller;
 
+import au.com.dto.IssueDto;
 import au.com.service.IssueTrackerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by amitsjoshi on 01/04/18.
@@ -14,4 +14,18 @@ public class IssueTrackerController
 {
     @Autowired
     private IssueTrackerService issueTrackerService;
+
+    @RequestMapping(value = "/issue/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public IssueDto getIssueById(@PathVariable(value="id") long id)
+    {
+        return issueTrackerService.getById(id);
+    }
+
+    @RequestMapping(value = "/issue", method = RequestMethod.POST)
+    @ResponseBody
+    public IssueDto createOrUpdateIssue(@RequestBody IssueDto issueDto)
+    {
+        return issueTrackerService.createOrUpdateIssue(issueDto);
+    }
 }
